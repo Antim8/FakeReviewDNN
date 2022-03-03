12be0cccdf76aa212b1088a378d45994cfc041ce
TimKapf <77025624+TimKapf@users.noreply.github.com>
2022-03-03 12:18:22 +0100

    functions added

--- data_preparation.py
+++ data_preparation.py
@@ -1,50 +1,59 @@
 from cProfile import label
 from random import shuffle
+from re import I
 import numpy as np
 import tensorflow as tf
 import pandas as pd
 
-# label for fake or real review
-label = pd.read_csv("fake_reviews_dataset.csv", usecols=[2]).values
 
-for [i], j in zip(label, range(len(label))):
-    if i == "CG":
-        label[j] = 1
-    else:
-        label[j] = 0
-label = np.squeeze(label, axis=1)
+def get_dataset():
 
-# The review 
-text = pd.read_csv("fake_reviews_dataset.csv", usecols=[3]).values
-text = np.squeeze(text, axis=1)
+    # label for fake or real review
+    # CG -> Computer-generated fake reviews; OR = Original reviews
+    label = pd.read_csv("fake_reviews_dataset.csv", usecols=[2]).values
 
-df = pd.DataFrame(columns=['text','label'])
+    # CG -> 1; OR -> 0
+    for [i], j in zip(label, range(len(label))):
+        if i == "CG":
+            label[j] = 1
+        else:
+            label[j] = 0
+    label = np.squeeze(label, axis=1)
 
-df['text'] = text
-df['label'] = label
+    # The review 
+    text = pd.read_csv("fake_reviews_dataset.csv", usecols=[3]).values
+    text = np.squeeze(text, axis=1)
 
-# Um die daten zu mischen (geht das?)
-df = df.sample(frac=1)
+    df = pd.DataFrame(columns=['text','label'])
+    df['text'] = text
+    df['label'] = label
+    # Shuffle the data, since it is sorted by categories
+    df = df.sample(frac=1)
 
-#Only train and test dataset so far, (maybe validation?)
-#Evtl. muss vorher auch geshuiffelt werden wegen den categrories 
-test_df = df[int(len(text)/2):]
-train_df = df[:int(len(text)/2)]
+    text_data = tokenizer(df['text'])
 
+    ds = tf.data.Dataset.from_tensor_slices((text_data, df['label'].values.astype(np.int32)))
 
-# Tokenizing 
-num_words = 8000
-oov_token = '<UNK>'
-maxlen = max(len(x) for x in train_df['text'])
-padding = 'post'
+    return ds
 
-tokenizer = tf.keras.preprocessing.text.Tokenizer(num_words=8000, oov_token=oov_token)
-tokenizer.fit_on_texts(train_df['text'])
+def tokenizer(text_data, num_words=8000, oov_token='<UNK>', maxlen=None, padding='post'):
 
-train_seq = tokenizer.texts_to_sequences(train_df['text'])
-test_seq = tokenizer.texts_to_sequences(test_df['text'])
+    if maxlen == None:
+        maxlen = max(len(x) for x in text_data)
 
-train_seq = tf.keras.preprocessing.sequence.pad_sequences(train_seq, maxlen=maxlen, padding=padding)
-test_seq = tf.keras.preprocessing.sequence.pad_sequences(test_seq, maxlen=maxlen, padding=padding)
+    tokenizer = tf.keras.preprocessing.text.Tokenizer(num_words=8000, oov_token=oov_token)
+    tokenizer.fit_on_texts(text_data)
 
+    text_seq = tokenizer.texts_to_sequences(text_data)
+    text_seq = tf.keras.preprocessing.sequence.pad_sequences(text_seq, maxlen=maxlen, padding=padding)
+    
+    return text_seq
+
+def data_pipeline(ds, shuffle=1000, batch=32, prefetch=20):
+    ds = ds.shuffle(shuffle)
+    ds = ds.batch(batch)
+    ds = ds.prefetch(prefetch)
+
+    return ds
+    
 
