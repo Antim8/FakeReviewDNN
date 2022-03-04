ba6cf23018b662dc1921ff5ef0ee0a9b9e2a5722
Antim8 <petersen.tim@outlook.de>
2022-03-04 22:52:03 +0100

    small adjustments

--- data_preparation.py
+++ data_preparation.py
@@ -17,34 +17,44 @@ def get_dataset():
             label[j] = 0
     label = np.squeeze(label, axis=1)
 
-    # The review 
+    # The review
     text = pd.read_csv("fake_reviews_dataset.csv", usecols=[3]).values
     text = np.squeeze(text, axis=1)
 
-    df = pd.DataFrame(columns=['text','label'])
-    df['text'] = text
-    df['label'] = label
+    df = pd.DataFrame(columns=["text", "label"])
+    df["text"] = text
+    df["label"] = label
     # Shuffle the data, since it is sorted by categories
     df = df.sample(frac=1)
 
-    text_data = tokenizer(df['text'])
+    text_data = tokenizer(df["text"])
 
-    ds = tf.data.Dataset.from_tensor_slices((text_data, df['label'].values.astype(np.int32)))
+    ds = tf.data.Dataset.from_tensor_slices(
+        (text_data, df["label"].values.astype(np.int32))
+    )
 
     return ds
 
-def tokenizer(text_data, num_words=8000, oov_token='<UNK>', maxlen=None, padding='post'):
+
+def tokenizer(
+    text_data, num_words=8000, oov_token="<UNK>", maxlen=None, padding="post"
+):
 
     if maxlen == None:
         maxlen = max(len(x) for x in text_data)
 
-    tokenizer = tf.keras.preprocessing.text.Tokenizer(num_words=8000, oov_token=oov_token)
+    tokenizer = tf.keras.preprocessing.text.Tokenizer(
+        num_words=8000, oov_token=oov_token
+    )
     tokenizer.fit_on_texts(text_data)
 
     text_seq = tokenizer.texts_to_sequences(text_data)
-    text_seq = tf.keras.preprocessing.sequence.pad_sequences(text_seq, maxlen=maxlen, padding=padding)
-    
+    text_seq = tf.keras.preprocessing.sequence.pad_sequences(
+        text_seq, maxlen=maxlen, padding=padding
+    )
+
     return text_seq
+
 
 def data_pipeline(ds, shuffle=1000, batch=32, prefetch=20):
     ds = ds.shuffle(shuffle)
@@ -52,5 +62,10 @@ def data_pipeline(ds, shuffle=1000, batch=32, prefetch=20):
     ds = ds.prefetch(prefetch)
 
     return ds
-    
 
+
+if __name__ == "__main__":
+    ds = get_dataset()
+    ds = data_pipeline(ds)
+    for i in ds:
+        print(i)
