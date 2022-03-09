00ffb37523b474e0b2c104793b3e48e5baeafe8b
Antim8 <petersen.tim@outlook.de>
2022-03-09 13:39:28 +0100

    try to implement pretrained set

---
+++ main.py
@@ -0,0 +1,31 @@
+
+
+def main():
+    
+    # Load the pretrained model from TF-Hub
+    # module = hub.Module("https://tfhub.dev/google/wiki40b-lm-en/1")
+    # -> embed in sublcassed model ?
+    
+    # configure model to our needs
+    
+    # prepare our fake review data for lm fine tuning
+    
+    # build lstm model
+    
+    # fine tune lstm for fake review data
+    
+    # fine tune the classifier for fake_review detection
+    
+    
+    # GENERATOR
+    #TODO
+    
+    
+    pass
+
+
+
+
+
+if __name__ == "__main__":
+    main()
